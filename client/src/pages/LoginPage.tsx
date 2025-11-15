4812d9479c01360260d4e6431358a54919b1724d
Lizaveta <sobaleva.lizaveta@gmail.com>
2025-11-15 22:32:05 +0100

    Add Firebase authentication

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,9 +1,11 @@
-import { Button, Card, Form, Input, Typography } from "antd";
+import { Button, Card, Form, Input, Typography, Space } from "antd";
 import { useNavigate } from "react-router-dom";
 import { login } from "../api/auth";
 import { useAuthStore } from "../store/authStore";
+import { loginWithGoogle, loginWithFacebook } from "../firebase";
+import { trackEvent } from "../analytics";
 
-const { Title } = Typography;
+const { Title, Text } = Typography;
 
 export default function LoginPage() {
   const navigate = useNavigate();
@@ -19,10 +21,31 @@ export default function LoginPage() {
     }
   };
 
+  const handleGoogle = async () => {
+    const fbUser = await loginWithGoogle();
+    setUser({
+      id: null,
+      firstName: fbUser.displayName || "Google",
+      lastName: "",
+      email: fbUser.email || "",
+    });
+    navigate("/", { replace: true });
+  };
+
+  const handleFacebook = async () => {
+    const fbUser = await loginWithFacebook();
+    setUser({
+      id: null,
+      firstName: fbUser.displayName || "Facebook",
+      lastName: "",
+      email: fbUser.email || "",
+    });
+    navigate("/", { replace: true });
+  };
+
   return (
     <Card style={{ maxWidth: 400, margin: "40px auto" }}>
       <Title level={3}>Login</Title>
-
       <Form layout="vertical" onFinish={onFinish}>
         <Form.Item
           label="Email"
@@ -31,7 +54,6 @@ export default function LoginPage() {
         >
           <Input />
         </Form.Item>
-
         <Form.Item
           label="Password"
           name="password"
@@ -39,13 +61,22 @@ export default function LoginPage() {
         >
           <Input.Password />
         </Form.Item>
-
         <Form.Item>
           <Button type="primary" htmlType="submit" block>
             Login
           </Button>
         </Form.Item>
       </Form>
+
+      <Text>Or sign in with</Text>
+      <Space direction="vertical" style={{ width: "100%", marginTop: 8 }}>
+        <Button onClick={handleGoogle} block>
+          Login with Google
+        </Button>
+        <Button onClick={handleFacebook} block>
+          Login with Facebook
+        </Button>
+      </Space>
     </Card>
   );
 }
